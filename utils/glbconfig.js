7e4dd7ed345678d5c91162f987a528aa0c07781d
刘勇进 <522585487@qq.com>
2023-09-27 14:10:52 +0800

    添加配置

--- utils/glbconfig.js
+++ utils/glbconfig.js
@@ -5,39 +5,39 @@ export function get_config() {
   return [
     {
       model_idnex:0,
-      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/glb/一带一路/沙漠骆驼9.25.glb", 
-      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/一带一路/images/1.png",
+      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/model/shamo_luotuo.glb", 
+      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/changebtn/1.png",
       size:5.01580
     }, 
     { 
       model_idnex:1,
-      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/glb/rabbit.glb", 
-      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/一带一路/images/2.png",
-      size:0.5
+      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/model/chelizhi_huolieniao.glb", 
+      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/changebtn/2.png",
+      size:6.50029
     }, 
     { 
       model_idnex:2,
-      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/glb/一带一路/沙漠骆驼9.25.glb", 
-      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/一带一路/images/3.png",
-      size:5.01580
+      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/model/baochuan.glb", 
+      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/changebtn/3.png",
+      size:7.50704
     }, 
     { 
       model_idnex:3,
-      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/glb/rabbit.glb", 
-      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/一带一路/images/4.png",
-      size:0.5
+      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/model/tielu.glb", 
+      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/changebtn/4.png",
+      size:33.54697
     }, 
     { 
       model_idnex:4,
-      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/glb/一带一路/沙漠骆驼9.25.glb", 
-      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/一带一路/images/5.png",
-      size:5.01580
+      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/model/banma_feizhou.glb", 
+      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/changebtn/5.png",
+      size:6.82142
     }, 
     { 
       model_idnex:5,
-      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/glb/rabbit.glb", 
-      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/一带一路/images/6.png",
-      size:0.5
+      glburl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/model/shenmiao_ganlanshu_hepingge.glb", 
+      iconurl:"https://mp-c34bf075-2376-4524-984d-4801256468f3.cdn.bspapp.com/yidaiyilu/changebtn/6.png",
+      size:15.43312
     }, 
   ]
 } 
